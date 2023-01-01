4da86dc400ae3b58646c50a091bba825bd1e958b
IsrailGulzar <68149527+israilgulzar@users.noreply.github.com>
2023-01-01 08:40:51 +0530

    test

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 5000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/test', (req, res) => {
+  res.send({
+    status: 200,
+    msg: 'good israil'
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
